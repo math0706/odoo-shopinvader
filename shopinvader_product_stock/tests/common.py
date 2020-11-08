f2f35243eda85b1a5365932e7dfd737c34736d9a
Sébastien BEAU <sebastien.beau@akretion.com>
2020-11-08 10:55:04 +0100

    [FIX] use last  lib version, remove direct import, and remove useless reload of testing model

--- shopinvader_product_stock/tests/common.py
+++ shopinvader_product_stock/tests/common.py
@@ -24,13 +24,6 @@ class StockCommonCase(TestBindingIndexBaseFake, JobMixin):
                 test_queue_job_no_delay=False,  # we want the jobs
             )
         )
-        cls.loader = FakeModelLoader(cls.env, cls.__module__)
-        cls.loader.backup_registry()
-        from odoo.addons.connector_search_engine.tests.models import SeBackendFake
-        cls.loader.update_registry(
-            (SeBackendFake, )
-        )
-        cls._create_fake_acl(SeBackendFake)
         ref = cls.env.ref
         cls.shopinvader_backend = ref("shopinvader.backend_1")
         cls.warehouse_1 = ref("stock.warehouse0")
